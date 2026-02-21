7156a3550d507b6e9c4a47cb265e02fd9f0db953
amit0006 <kumaramitthakur563@gmail.com>
2026-02-21 22:24:41 +0530

    Group Anagrams

---
+++ Group Anagrams.java
@@ -0,0 +1,44 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Explanation:
+
+// There is no string in strs that can be rearranged to form "bat".
+// The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+// The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+// Example 2:
+
+// Input: strs = [""]
+
+// Output: [[""]]
+
+// Example 3:
+
+// Input: strs = ["a"]
+
+// Output: [["a"]]
+
+
+// code:
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            char a[] = s.toCharArray();
+            Arrays.sort(a);
+            String key = new String(a);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+        
+    }
+}
